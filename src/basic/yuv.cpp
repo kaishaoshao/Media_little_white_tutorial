3e0f9bac22e9cb8deb6a7aa21944b15d82d3f771
kaishaoshao <2032751077@qq.com>
2024-08-20 00:06:48 +0800

    Update  yuv.cpp

--- src/basic/yuv.cpp
+++ src/basic/yuv.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <math.h>
 
 
 //  分离YUV420P像素数据中的Y、U、V分量
@@ -258,7 +258,7 @@ int Border_yuv420(const char *url, int width, int height,int border,int num){
 				}
 			}
 		}
-		fwrite(pic,1,size,fp1);
+		fwrite(pic,1,size,fp_b);
 	}
  
 	free(pic);
@@ -278,16 +278,16 @@ int Border_yuv420(const char *url, int width, int height,int border,int num){
 // @param barnum   Number of bars
 // @param url_out  Location of Output YUV file.
 ///////////////////////////////////////////////
-int Graybar_yuv420(int width, int height,int ymin,int ymax,int barnum,char *url_out){
-	int barwidth;
-	float lum_inc;
+int Graybar_yuv420(int width, int height,int ymin,int ymax,int barnum,const char *url_out){
+	int barwidth;               // 每条灰度条宽度
+	float lum_inc;              // 亮度
 	unsigned char lum_temp;
-	int uv_width,uv_height;
+	int uv_width,uv_height;     // uv的宽高
 	FILE *fp=NULL;
 	unsigned char *data_y=NULL;
 	unsigned char *data_u=NULL;
 	unsigned char *data_v=NULL;
-	int t=0,i=0,j=0;
+
  
 	barwidth = width / barnum;      
 	lum_inc=((float)(ymax-ymin))/((float)(barnum-1));
@@ -304,25 +304,29 @@ int Graybar_yuv420(int width, int height,int ymin,int ymax,int barnum,char *url_
  
 	//Output Info
 	printf("Y, U, V value from picture's left to right:\n");
-	for(t=0;t<(width/barwidth);t++){
+	for(int t=0;t<(width/barwidth);t++){
 		lum_temp=ymin+(char)(t*lum_inc);
 		printf("%3d, 128, 128\n",lum_temp);
 	}
+
 	//Gen Data
-	for(j=0;j<height;j++){
-		for(i=0;i<width;i++){
-			t=i/barwidth;
+    // Y
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
+			int t=i/barwidth;
 			lum_temp=ymin+(char)(t*lum_inc);
 			data_y[j*width+i]=lum_temp;
 		}
 	}
-	for(j=0;j<uv_height;j++){
-		for(i=0;i<uv_width;i++){
+    // U
+	for(int j=0;j<uv_height;j++){
+		for(int i=0;i<uv_width;i++){
 			data_u[j*uv_width+i]=128;
 		}
 	}
-	for(j=0;j<uv_height;j++){
-		for(i=0;i<uv_width;i++){
+    // V
+	for(int j=0;j<uv_height;j++){
+		for(int i=0;i<uv_width;i++){
 			data_v[j*uv_width+i]=128;
 		}
 	}
@@ -336,7 +340,43 @@ int Graybar_yuv420(int width, int height,int ymin,int ymax,int barnum,char *url_
 	return 0;
 }
 
-//  计算两个YUV420P像素数据的PSNR
+///////////////////////////////////////////////////////
+// Calculate PSNR between 2 YUV420P file
+// @param url1     Location of first Input YUV file.
+// @param url2     Location of another Input YUV file.
+// @param w        Width of Input YUV file.
+// @param h        Height of Input YUV file.
+// @param num      Number of frames to process.
+///////////////////////////////////////////////////////
+int Psnr_yuv420(char *url1,char *url2,int w,int h,int num){
+	FILE *fp1=fopen(url1,"rb+");
+	FILE *fp2=fopen(url2,"rb+");
+	unsigned char *pic1=(unsigned char *)malloc(w*h);
+	unsigned char *pic2=(unsigned char *)malloc(w*h);
+ 
+	for(int i=0;i<num;i++){
+		fread(pic1,1,w*h,fp1);
+		fread(pic2,1,w*h,fp2);
+ 
+		double mse_sum=0,mse=0,psnr=0;
+		for(int j=0;j<w*h;j++){
+			mse_sum+=pow((double)(pic1[j]-pic2[j]),2);
+		}
+		mse=mse_sum/(w*h);
+		psnr=10*log10(255.0*255.0/mse);
+		printf("%5.3f\n",psnr);
+ 
+		fseek(fp1,w*h/2,SEEK_CUR);
+		fseek(fp2,w*h/2,SEEK_CUR);
+ 
+	}
+ 
+	free(pic1);
+	free(pic2);
+	fclose(fp1);
+	fclose(fp2);
+	return 0;
+}
 //  分离RGB24像素数据中的R、G、B分量
 //  将RGB24格式像素数据封装为BMP图像
 //  将RGB24格式像素数据转换为YUV420P格式像素数据
@@ -350,5 +390,7 @@ int main(int argc,char* argv[])
     // Convert_yuv420_gray("../../res/basic/pic/lena_256x256_yuv420p.yuv",256,256,1);
     // Halfy_yuv420("../../res/basic/pic/lena_256x256_yuv420p.yuv",256,256,1);
     // Border_yuv420("../../res/basic/pic/lena_256x256_yuv420p.yuv",256,256,20,1);
+    // Graybar_yuv420(640, 360,0,255,10,"../../res/basic/pic/graybar_640x360.yuv");  x
+    
     return 0;
 }  
